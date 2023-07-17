fc793bdf71dd5d50b8f58948268a8456ae2718bf
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-18 07:46:36 +0900

    apply mix format

--- test/nx_atan_table_test.exs
+++ test/nx_atan_table_test.exs
@@ -4,7 +4,7 @@ defmodule NxAtanTableTest do
 
   test "NxAtanTable.table" do
     n = 16
-    assert NxAtanTable.Math.table([bit: 32], [n: n]) == NxAtanTable.Math.table([bit: 32], [n: n])
-    assert NxAtanTable.Math.table([bit: 64], [n: n]) == NxAtanTable.Math.table([bit: 64], [n: n])
+    assert NxAtanTable.Math.table([bit: 32], n: n) == NxAtanTable.Math.table([bit: 32], n: n)
+    assert NxAtanTable.Math.table([bit: 64], n: n) == NxAtanTable.Math.table([bit: 64], n: n)
   end
 end
