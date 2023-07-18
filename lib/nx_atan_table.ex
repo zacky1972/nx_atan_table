748198df0414dab9cf5d7d0a2ccc1f114d4eaa14
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-18 10:16:11 +0900

    implement temporally

--- lib/nx_atan_table.ex
+++ lib/nx_atan_table.ex
@@ -8,12 +8,6 @@ defmodule NxAtanTable do
   $2^{b - 2} \\arctan(2^{-i})$ where $0 \\leq i \\leq n - 1$,
   which is `b` bits.
   """
-  def table(bit_opts \\ [], opts \\ []) do
-    Nx.linspace(0, opts[:n] - 1, opts)
-    |> then(&Nx.pow(2, Nx.negate(&1)))
-    |> Nx.atan()
-    |> Nx.multiply(2 ** (bit_opts[:bit] - 2))
-    |> Nx.floor()
-    |> Nx.as_type({:s, bit_opts[:bit]})
+  def table(_n, _b) do
   end
 end
