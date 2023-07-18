0863bb2de7571f422a3e5d6b7dae50b6ebe011a5
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-19 06:22:11 +0900

    test: enhanced testing

--- test/nx_atan_table_test.exs
+++ test/nx_atan_table_test.exs
@@ -2,9 +2,15 @@ defmodule NxAtanTableTest do
   use ExUnit.Case
   doctest NxAtanTable
 
-  test "NxAtanTable.table" do
-    n = 16
-    assert NxAtanTable.table(n, 32) == NxAtanTable.Math.table([bit: 32], n: n)
-    assert NxAtanTable.table(n, 64) == NxAtanTable.Math.table([bit: 64], n: n)
+  describe "NxAtanTable.table" do
+    test "32bit" do
+      n = 32
+      assert NxAtanTable.table(n, 32) == NxAtanTable.Math.table([bit: 32], n: n)
+    end
+
+    test "64bit" do
+      n = 64
+      assert NxAtanTable.table(n, 64) == NxAtanTable.Math.table([bit: 64], n: n)
+    end
   end
 end
