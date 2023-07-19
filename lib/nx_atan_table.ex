dfdf45262096f9a3e8dc4a0cdabf948e9424a29f
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-20 06:04:24 +0900

    apply mix format

--- lib/nx_atan_table.ex
+++ lib/nx_atan_table.ex
@@ -16,7 +16,7 @@ defmodule NxAtanTable do
       n -> {n - 1, n - 1}
     end)
     |> Stream.map(fn n -> Bitwise.bsl(1, n) end)
-    |> Stream.map(& atan_of_reciprocal(&1, b))
+    |> Stream.map(&atan_of_reciprocal(&1, b))
     |> Enum.reverse()
     |> Nx.tensor(type: {:s, b})
   end
@@ -55,32 +55,35 @@ defmodule NxAtanTable do
     {:reply, r1, state} = cache_atan_of_reciprocal({49, b}, state, Map.get(state, {49, b}))
     {:reply, r2, state} = cache_atan_of_reciprocal({57, b}, state, Map.get(state, {57, b}))
     {:reply, r3, state} = cache_atan_of_reciprocal({239, b}, state, Map.get(state, {239, b}))
-    {:reply, r4, _state} = cache_atan_of_reciprocal({110443, b}, state, Map.get(state, {110443, b}))
-    (12 * r1 + 32 * r2 - 5 * r3 + 12 * r4)
+
+    {:reply, r4, _state} =
+      cache_atan_of_reciprocal({110_443, b}, state, Map.get(state, {110_443, b}))
+
+    12 * r1 + 32 * r2 - 5 * r3 + 12 * r4
   end
 
   defp atan_of_reciprocal_s(2, state, b) do
     {:reply, r1, state} = cache_atan_of_reciprocal({3, b}, state, Map.get(state, {3, b}))
     {:reply, r2, _state} = cache_atan_of_reciprocal({7, b}, state, Map.get(state, {7, b}))
-    (r1 + r2)
+    r1 + r2
   end
 
   defp atan_of_reciprocal_s(3, state, b) do
     {:reply, r1, state} = cache_atan_of_reciprocal({5, b}, state, Map.get(state, {5, b}))
     {:reply, r2, _state} = cache_atan_of_reciprocal({8, b}, state, Map.get(state, {8, b}))
-    (r1 + r2)
+    r1 + r2
   end
 
   defp atan_of_reciprocal_s(7, state, b) do
     {:reply, r1, state} = cache_atan_of_reciprocal({8, b}, state, Map.get(state, {8, b}))
     {:reply, r2, _state} = cache_atan_of_reciprocal({57, b}, state, Map.get(state, {57, b}))
-    (r1 + r2)
+    r1 + r2
   end
 
   defp atan_of_reciprocal_s(57, state, b) do
     {:reply, r1, state} = cache_atan_of_reciprocal({32, b}, state, Map.get(state, {32, b}))
     {:reply, r2, _state} = cache_atan_of_reciprocal({73, b}, state, Map.get(state, {73, b}))
-    (r1 - r2)
+    r1 - r2
   end
 
   defp atan_of_reciprocal_s(n, _state, bit) when n > 0 and n < Bitwise.bsl(1, bit - 1) do
--- test/nx_atan_table_test.exs
+++ test/nx_atan_table_test.exs
@@ -8,13 +8,27 @@ defmodule NxAtanTableTest do
     test "32bit" do
       b = 32
       n = 32
-      assert NxAtanTable.Math.equals_with_epsilon(NxAtanTable.table(n, b), NxAtanTable.Math.table([bit: b], n: n), @epsilon, b) |> Nx.to_number() == 1
+
+      assert NxAtanTable.Math.equals_with_epsilon(
+               NxAtanTable.table(n, b),
+               NxAtanTable.Math.table([bit: b], n: n),
+               @epsilon,
+               b
+             )
+             |> Nx.to_number() == 1
     end
 
     test "64bit" do
       b = 64
       n = 64
-      assert NxAtanTable.Math.equals_with_epsilon(NxAtanTable.table(n, b), NxAtanTable.Math.table([bit: b], n: n), @epsilon, b) |> Nx.to_number() == 1
+
+      assert NxAtanTable.Math.equals_with_epsilon(
+               NxAtanTable.table(n, b),
+               NxAtanTable.Math.table([bit: b], n: n),
+               @epsilon,
+               b
+             )
+             |> Nx.to_number() == 1
     end
   end
 end
